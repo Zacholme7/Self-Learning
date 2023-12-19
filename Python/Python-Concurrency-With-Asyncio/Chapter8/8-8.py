f1e12ea19f8864d535129eceb8e877c74334223e
ZacHolme7 <zacholme@gmail.com>
2023-12-19 09:33:26 -0600

    daily reading

---
+++ Python/Python-Concurrency-With-Asyncio/Chapter8/8-8.py
@@ -0,0 +1,30 @@
+# 8.8: Reading input one character at a time
+import sys
+from asycio import StreamReader
+from collections import deque
+from util import move_back_one_char, clear_line
+
+async def read_line(stdin_reader: StreamReader) -> str:
+    # convenience function to delete the previous character from standard output
+    def erase_last_char():
+        move_back_one_char()
+        sys.stdout.write(' ')
+        move_back_one_char()
+
+    delete_char = b'\x7f'
+    input_buffer = deque()
+
+    while (input_char := await stdin_reader.read(1)) != b'\n':
+        if input_char == delete_char:
+            if len(input_buffer) > 0:
+                input.buffer.pop()
+                erase_last_char()
+                sys.stdout.flush()
+        else:
+            input_buffer.append(input_char)
+            sys.stdout.write(input_char.decode())
+            sys.stdout.flush()
+    clear_line()
+    return b''.join(input_buffer).decode()
+
+
--- Python/Python-Concurrency-With-Asyncio/Chapter8/util/__init__.py
+++ Python/Python-Concurrency-With-Asyncio/Chapter8/util/__init__.py
@@ -2,4 +2,5 @@ from .delay_function import delay
 from .async_timer import async_timed
 from .fetch_status import fetch_status
 from .create_stdin_reader import create_stdin_reader
+from .convenience_functions import save_cursor_position, restore_cursor_position, move_to_top_of_screen, delete_line, clear_line, move_back_one_char, move_to_bottom_of_screen
 
