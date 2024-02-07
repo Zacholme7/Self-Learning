aeb74d0f87eaaa366b0920918e5dbaacd7bf67c6
ZacHolme7 <zacholme@gmail.com>
2024-02-06 20:00:24 -0600

    rust reading

--- Rust/Rust-For-Rustaceans/1-Foundations/exploration/src/main.rs
+++ Rust/Rust-For-Rustaceans/1-Foundations/exploration/src/main.rs
@@ -29,14 +29,38 @@ fn main() {
     let x2 = x1; // x1 is copy so it was not moved into z and copied, therefore we can still use it
     // let y2 = y1; box is not copy so y1 was dropped when it was moved 
     
-
     let mut x = 42;
     let z = 10;
     let mut y = &mut x;
-    *y = 10;
 
-
+    let mut x = Box::new(10);
+    replace_with_84(&mut x);
+    println!("{}", x);
 
 
 
 }
+
+fn replace_with_84(s: &mut Box<i32>) {
+    // this is not okay, *s would be empty
+    // let was = *s; *s = Box<i32> and we cant move this box out of x since we just have a ref,
+    // caller would think that they still owned the value
+    // but this is
+    let was = std::mem::take(s); // this will return the value in the box and default init it
+                                 // equivalent to std;:mem::replace(&mut value, Default::default())
+    // at this point, s(x) is 0
+    // so is this
+    *s = was; // this will assign s back to its original spot, moving was and dropping both s prev
+              // value and was
+
+    // we can exchange values behind &mut
+    let mut r = Box::new(84);
+    std::mem::swap(s, &mut r);
+    assert_ne!(*r, 84);
+}
+
+
+
+
+
+
